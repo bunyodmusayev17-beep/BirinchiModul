956dd2736800b1ab33568170b0556198f6fbbf05
bunyodmusayev17-beep <bunyodmusayev17@gmail.com>
2025-12-06 13:55:11 +0500

    done

---
+++ Uygavazifa/uygavazifa1/uygavazifa1/Program.cs
@@ -0,0 +1,645 @@
+﻿namespace uygavazifa1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for( int i = 1; i <= 20; i++)
+            //  {
+            //      Console.WriteLine(i);
+            //  }
+
+
+            //                                          2-masala
+
+            //for(int i = 25; i <= 100; i++)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+
+            //                                          3-masala
+
+
+
+            //for (int i = 12; i <= 200; i++)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+            // 4-masala
+
+
+
+            //for (int i = 15; i > 2; i--)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+
+            //                                           5-masala
+
+            //for (int i = 120; i >= 23; i--)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+
+            //                                           6-masala
+
+            //for (int i = 10; i < 50; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.Write($"{i} ");
+            //    }
+
+
+            //                                          7-masala
+
+
+            //for (int i = 120; i < 250; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.Write($"{i} ");
+            //    }
+
+
+
+
+
+            //                                              8 - masala
+
+
+            //for (int i = 100; i >= 50; i--)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                          9 - masala
+
+
+            //for (int i = 80; i >= 20; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                      10 - masala
+
+
+            //int a = 5, b = 40;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                          11 - masala
+
+
+            //a = 10;
+            //b = 200;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 4 == 0 && i % 7 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                           12 - masala
+
+
+            //a = 10;
+            //b = 200;
+            //int count = 0;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 8 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+
+            //                                          13 - masala
+
+            //a = 10;
+            //b = 60;
+            //for (int i = b; i >= a; i--)
+            //{
+            //    if (i % 9 == 0 || i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                          14 - masala
+
+            //int sum = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+            //                                      15 - masala
+
+
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int a = i / 100;
+            //    int b = (i / 10) % 10;
+            //    int c = i % 10;
+            //    if (a + b + c > 20)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                      16 - masala
+
+
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int a = i / 100;
+            //    int c = i % 10;
+            //    if (a == c)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                      17 - masala
+
+
+            //int count17 = 0, sum17 = 0;
+            //for (int i = 10; i <= 99; i++)
+            //{
+            //    int a = i / 10;
+            //    int b = i % 10;
+            //    if (a * b > 12)
+            //    {
+            //        count17++;
+            //        sum17 += i;
+            //    }
+            //}
+            //Console.WriteLine(count17);
+            //Console.WriteLine(sum17);
+
+            //                                       18 - masala
+
+
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int b = (i / 10) % 10;
+            //    if (b == 2 || b == 5)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                      19 - masala
+
+
+            //int a19 = 5;
+            //for (int i = 1; i <= a19; i++)
+            //{
+            //    Console.WriteLine(a19);
+            //}
+
+            //                                      20 - masala
+
+
+            //int a20 = 3, b20 = 6;
+            //for (int i = 1; i <= b20; i++)
+            //{
+            //    Console.WriteLine(a20);
+            //}
+
+
+            ////                                     21 - masala
+
+
+            //int a21 = 4, b21 = 9;
+            //for (int i = 1; i <= a21; i++)
+            //{
+            //    Console.WriteLine(b21);
+            //}
+
+
+            //                                              22 - masala
+
+
+            //int a22 = 36;
+            //for (int i = 1; i <= a22; i++)
+            //{
+            //    if (a22 % i == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //                                      23 - masala
+
+
+            //int a23 = 40, count23 = 0;
+            //for (int i = 1; i <= a23; i++)
+            //{
+            //    if (a23 % i == 0)
+            //    {
+            //        count23++;
+            //    }
+            //}
+            //Console.WriteLine(count23);
+
+            //                                          24 - masala
+
+
+            //int a24 = 30, sum24 = 0;
+            //for (int i = 1; i <= a24; i++)
+            //{
+            //    if (a24 % i == 0)
+            //    {
+            //        sum24 += i;
+            //    }
+            //}
+            //Console.WriteLine(sum24);
+
+
+            // 1.3.2
+
+            //                                          1 - masala  
+
+
+            //int n1 = 12345;
+            //int count1 = 0;
+            //while (n1 > 0)
+            //{
+            //    n1 /= 10;
+            //    count1++;
+            //}
+            //Console.WriteLine(count1);
+
+
+
+            //                                          2 - masala  
+            //int n2 = 5678;
+            //int sum2 = 0;
+            //while (n2 > 0)
+            //{
+            //    sum2 += n2 % 10;
+            //    n2 /= 10;
+            //}
+            //Console.WriteLine(sum2);
+
+            //                                          3 - masala  
+
+
+            //int n3 = 48291;
+            //int count3 = 0;
+            //while (n3 > 0)
+            //{
+            //    if ((n3 % 10) % 2 == 0) count3++;
+            //    n3 /= 10;
+            //}
+            //Console.WriteLine(count3);
+
+
+            //                                          4 - masala  
+
+
+            //int n4 = 48291;
+            //int count4 = 0;
+            //while (n4 > 0)
+            //{
+            //    if ((n4 % 10) % 2 == 1) count4++;
+            //    n4 /= 10;
+            //}
+            //Console.WriteLine(count4);
+
+
+            //                                      5 - masala  
+
+
+            //int x5 = 1, count5 = 0;
+            //while (x5 != 0)
+            //{
+            //    x5 = int.Parse(Console.ReadLine());
+            //    if (x5 != 0) count5++;
+            //}
+            //Console.WriteLine(count5);
+
+
+            //                                      6 - masala  
+
+
+            //int x6 = 1, count6 = 0;
+            //while (x6 != 0)
+            //{
+            //    x6 = int.Parse(Console.ReadLine());
+            //    if (x6 > 10) count6++;
+            //}
+            //Console.WriteLine(count6);
+
+
+            //                                      7 - masala  
+
+
+            //int x7 = 1, sum7 = 0;
+            //while (x7 != 0)
+            //{
+            //    x7 = int.Parse(Console.ReadLine());
+            //    sum7 += x7;
+            //}
+            //Console.WriteLine(sum7);
+
+
+            //                                          8 - masala  
+
+
+
+            //int x8 = 1;
+            //int p8 = 1;
+            //while (x8 != 0)
+            //{
+            //    x8 = int.Parse(Console.ReadLine());
+            //    if (x8 != 0) p8 *= x8;
+            //}
+            //Console.WriteLine(p8);
+
+            //                                                  9 - masala 
+
+
+            //int x9 = 1, sum9 = 0;
+            //while (x9 != 0)
+            //{
+            //    x9 = int.Parse(Console.ReadLine());
+            //    if (x9 > 50) sum9 += x9;
+            //}
+            //Console.WriteLine(sum9);
+
+            //                                               10 - masala  
+
+
+            //int x10 = 1, count10 = 0;
+            //while (x10 % 2 != 0)
+            //{
+            //    x10 = int.Parse(Console.ReadLine());
+            //    count10++;
+            //}
+            //Console.WriteLine(count10 - 1);
+
+            //                                              11 - masala  
+
+
+            //int n11 = 123456;
+            //bool bor11 = false;
+            //while (n11 > 0)
+            //{
+            //    if (n11 % 10 == 5)
+            //    {
+            //        bor11 = true;
+            //        break;
+            //    }
+            //    n11 /= 10;
+            //}
+            //Console.WriteLine(bor11);
+
+            //                                                      12 - masala  
+
+
+            //int n12 = 13979;
+            //bool bor12 = false;
+            //while (n12 > 0)
+            //{
+            //    if ((n12 % 10) % 2 == 0)
+            //    {
+            //        bor12 = true;
+            //        break;
+            //    }
+            //    n12 /= 10;
+            //}
+            //Console.WriteLine(bor12);
+
+            //                                           13 - masala  
+
+
+            //int n13 = 81;
+            //bool daraja = true;
+
+            //while (n13 > 1)
+            //{
+            //    if (n13 % 3 != 0)
+            //    {
+            //        daraja = false;
+            //        break;
+            //    }
+            //    n13 /= 3;
+            //}
+            //Console.WriteLine(daraja);
+
+
+            //                                         14 - masala
+
+            //for (int i = 2; i <= 9; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.WriteLine($"{i} * {j} = {i * j}");
+            //    }
+
+
+            //                                           15 - masala
+
+            //int a = 2;
+            //int b = 8;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write($"{i}",j);
+
+
+            //                                           16 - masala
+
+
+            //for (int i = 2; i <= 100; i++)
+            //{
+            //    bool tub = true;
+            //    for (int j = 2; j * j <= i; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            tub = false;
+            //            break;
+            //        }
+            //    }
+            //    if (tub)
+            //    {
+            //        Console.Write($"{i} ");
+
+
+            //                                           17 - masala
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+
+            //if (a == b || a > b)
+            //{
+            //    Console.WriteLine("A < B bolishi lozim");
+            //}
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    bool tub = true;
+            //    for (int j = 2; j * j <= i; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            tub = false;
+            //            break;
+            //        }
+            //    }
+            //    if (tub)
+            //    {
+            //        Console.Write($"{i} ");
+
+
+            //                                           17 - masala
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (i == 1 || i == n)
+            //    {
+            //        for (int k = 0; k < n; k++)
+            //        {
+            //            Console.Write("*");
+            //            if (k < n - 1) Console.Write(" ");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.Write(new string(' ', (i - 1) * 2));
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //    }
+
+
+
+            //                                           18 - masala
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //        if (j < i - 1) Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //                                           19 - masala
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write(new string(' ', i * 2));
+            //    int stars = n - i;
+            //    for (int s = 0; s < stars; s++)
+            //    {
+            //        Console.Write("*");
+            //        if (s < stars - 1) Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //                                           20 - masala
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write(new string(' ', (n - i) * 2));
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //        if (j < i - 1) Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            //                                           21 - masala
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.Write(i);
+            //        if (j < n - 1) Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //                                           22 - masala
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write(new string(' ', (i - 1) * 2));
+            //    Console.Write(i);
+            //    Console.WriteLine();
+            //}
+
+
+            //                                           23 - masala
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write(i);
+            //        if (j < i - 1) Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+
+            //                                           24 - masala
+
+        }
+    }
+}
