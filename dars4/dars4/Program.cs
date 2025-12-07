ff4a0a79635e83b26625cc9f2f1c9ad5d4b171e4
bunyodmusayev17-beep <bunyodmusayev17@gmail.com>
2025-12-07 23:18:14 +0500

    dars4

---
+++ dars4/dars4/Program.cs
@@ -0,0 +1,208 @@
+﻿namespace dars4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.Write("Ismingizni kiriting: ");
+            //string name = Console.ReadLine()!;
+            //var natija = helloname(name);
+
+            //static string helloname(string name)
+            //{
+            //    return "hello " + name;
+            //}
+
+
+            //Console.WriteLine(natija);
+
+
+            //                                      2-masala
+
+
+            //Console.Write("Str1: ");
+            //var str1 = Console.ReadLine()!;
+            //Console.Write("Str2: ");
+            //var str2 = Console.ReadLine()!;
+
+
+
+
+
+            //static string doubling(string str1, string str2)
+            //{
+            //    return str1 + str2 + str2 + str1;
+            //}
+
+            //Console.WriteLine(doubling(str1,str2));
+
+
+            //                                      3-masala
+
+            //Console.Write("str1: ");
+            //var str1 = Console.ReadLine()!;
+            //Console.Write("str2: ");
+            //var str2 = Console.ReadLine()!;
+
+
+            //static string makeOutWord(string str1, string str2)
+            //{
+
+            //    if (str1.Length != 4)
+            //    {
+            //        Console.WriteLine("Birinchi str uznligi 4 bolishi shart");
+            //    }
+
+
+            //    return str1.Substring(0, 2) + str2 + str1.Substring(2);
+            //}
+
+            //Console.WriteLine(makeOutWord(str1, str2));
+
+
+
+            //                                      4-masala
+
+            //Console.Write("Matn kiriting: ");
+            //var str = Console.ReadLine()!;
+
+
+            //if (str.Length <= 2)
+            //{
+            //    Console.WriteLine("Str uzunligi kamida 2 bolishi lozim!!");
+
+            //}
+
+
+            //static string extraEnd(string str)
+            //{
+
+            //    var res = str.Substring(str.Length - 2);
+            //    return res + res + res;
+            //}
+            //Console.WriteLine(extraEnd(str));
+
+
+            //                                      5 -masala
+
+            //Console.Write("Matn kiriting: ");
+            //var str = Console.ReadLine()!;
+
+            //static string firstTwo(string str)
+            //{
+            //    return str.Substring(0, 2);
+            //}
+            //Console.WriteLine(firstTwo(str));
+
+
+
+            //                                      6 -masala
+
+
+            //Console.Write("Matn kiriting: ");
+            //var str = Console.ReadLine()!;
+            //var result = firstHalf(str);
+
+            //static string firstHalf(string str)
+            //{
+            //    var res = str.Length / 2;
+            //    return str.Substring(0, res);
+            //}
+            //Console.WriteLine(result);
+
+            //                                      7 -masala
+
+            //Console.Write("Matn kiriting: ");
+            //var str = Console.ReadLine()!;
+            //var res = withoutEnd(str);
+
+            //Console.WriteLine(res);
+
+            //static string withoutEnd(string str)
+            //{
+            //    return str.Substring(1, str.Length - 1);
+            //}
+
+            //                                      8 -masala
+
+            //Console.Write("Matn kiriting: ");
+            //var b = Console.ReadLine()!;
+            //Console.Write("Matn kiriting: ");
+            //var a = Console.ReadLine()!;
+            //var res = comboString(a, b);
+
+
+            //Console.WriteLine(res);
+
+
+            //static string comboString(string a, string b)
+            //{
+            //    var str = string.Empty;
+
+            //    if (a.Length > b.Length)
+            //    {
+            //        str = b + a + b;
+            //    }
+            //    else
+            //    {
+            //        str = a + b + a;
+            //    }
+
+            //    return str;
+            //}
+
+
+
+            //                                      9 -masala
+
+
+            //Console.Write("Matn kiriting: ");
+            //var str1 = Console.ReadLine()!;
+
+            //Console.Write("Matn kiriting: ");
+            //var str2 = Console.ReadLine()!;
+
+            //var res = nonStart(str1,str2!);
+
+            //static string nonStart(string str1, string str2)
+            //{
+            //    var res1 = string.Empty;
+            //    res1 = str1.Substring(1);
+            //    var res2 = string.Empty;
+            //    res2 = str2.Substring(1);
+
+            //    return res1+res2;
+            //}
+            //Console.WriteLine(res);
+
+            //                                      10 -masala
+
+
+            //Console.Write("Matn kiriting: ");
+            //var str = Console.ReadLine()!;
+
+            //var res = left2(str);
+
+            //static string left2(string str)
+            //{
+
+            //    if( str.Length <= 2)
+            //    {
+            //        return str;
+            //    }
+            //    var first2 = str.Substring(0, 2);
+            //    var left = str.Substring(2);
+
+            //    return left + first2;
+
+
+            //}
+            //Console.WriteLine(res);
+
+            //                                      11 -masala
+
+
+        }
+    }
+}
